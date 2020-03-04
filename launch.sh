710080bad061fc26778b26675c63a5a9f780d099
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2020-03-04 17:05:20 +0100

    error input, factorization

--- launch.sh
+++ launch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 DIR="../minishell"
-OUTPUT_DIR="output"
 
+OUTPUT_DIR="output"
 OK=$'\033[38;5;46m'
+OPT=$'\033[38;5;11m'
 FAIL=$'\033[38;5;9m'
 RESET=$'\033[0m'
 
@@ -14,35 +15,53 @@ function print_color() {
 function read_test {
 	while IFS= read -r line
 	do
-		echo "$line" | bash >> "$OUTPUT_DIR/$1_bash"
-		echo "$line" | ./minishell >> "$OUTPUT_DIR/$1_minishell" || return 1
+		echo "$line" | bash 2>> "$OUTPUT_DIR/$1_err_bash"  >> "$OUTPUT_DIR/$1_bash"
+		echo "$line" | ./minishell 2>> "$OUTPUT_DIR/$1_err_minishell" >> "$OUTPUT_DIR/$1_minishell" || return 1
 	done < "test/$1"
 	return 0
+}
+
+function check_std_output {
+	echo -en "Standar output\t: "
+	DIFF=$(diff $OUTPUT_DIR/$1_bash $OUTPUT_DIR/$1_minishell)
+	if [ "$DIFF" = "" ]
+	then
+		print_color "[OK]" $OK
+		rm -rf "$OUTPUT_DIR/$1_bash"
+		rm -rf "$OUTPUT_DIR/$1_minishell" 
+	else
+		print_color "[KO]" $FAIL
+	fi
+}
+
+function check_err_output {
+	echo -en "Error output\t: "
+	DIFF=$(diff $OUTPUT_DIR/$1_err_bash $OUTPUT_DIR/$1_err_minishell)
+	if [ "$DIFF" = "" ]
+	then
+		print_color "[OK]" $OK
+		rm -rf "$OUTPUT_DIR/$1_err_bash"
+		rm -rf "$OUTPUT_DIR/$1_err_minishell" 
+	else
+		print_color "[KO]" $OPT
+	fi
 }
 
 function lauch_test {
 	if [ -z "$1" ]; then
 		return 1
 	fi
-	echo $1
-	ERROR=$(read_test "$1")
-	#while IFS= read -r line
-	#do
-	#	echo "$line" | bash >> "$OUTPUT_DIR/$1_bash"
-	#	echo "$line" | ./minishell >> "$OUTPUT_DIR/$1_minishell" || (ERROR="ERROR" && break)
-	#done < "test/$1"
-	if [ "$ERROR" = "0" ]; then
-		echo "[FAILURE]"
-	fi
-	DIFF=$(diff $OUTPUT_DIR/$1_bash $OUTPUT_DIR/$1_minishell)
-	if [ "$DIFF" = "" ]
-	then
-		print_color "[OK]" $OK
-		#rm -rf "$OUTPUT_DIR/$1_bash"
-		#rm -rf "$OUTPUT_DIR/$1_minishell" 
+	echo -e "\n"$1 | tr a-z A-Z
+	echo -en "Execution\t: "
+	read_test "$1"
+	ERROR=$?
+	if [ "$ERROR" = "1" ]; then
+		print_color "[FAILURE]" $FAIL
 	else
-		echo "[KO]"
+		print_color "[OK]" $OK
 	fi
+	check_std_output $1
+	check_err_output $1
 }
 
 make -C $DIR
